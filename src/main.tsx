f685273c41cf75f03fbf5bd7990d7f09b81ef926
Sebastian Resendiz <sebas.r.2599@gmail.com>
2024-06-03 02:14:57 -0600

    Disabled devtools for react query

--- src/main.tsx
+++ src/main.tsx
@@ -35,7 +35,7 @@ ReactDOM.createRoot(document.getElementById('root') as HTMLElement).render(
           />
           <QueryClientProvider client={queryClient}>
             {/* The rest of your application */}
-            <ReactQueryDevtools initialIsOpen={false} />
+            {/* <ReactQueryDevtools initialIsOpen={false} /> */}
           </QueryClientProvider>
         </BrowserRouter>
       </QueryClientProvider>
