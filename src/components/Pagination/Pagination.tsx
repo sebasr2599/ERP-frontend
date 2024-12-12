bd63d45173b2a5d16eb4ae1c7b494ddef65b8b79
Sebastian Resendiz <sebas.r.2599@gmail.com>
2024-12-11 21:51:10 -0600

    Moved Pagination into custom component

---
+++ src/components/Pagination/Pagination.tsx
@@ -0,0 +1,23 @@
+import { IconButton } from '@mui/material';
+import { FC } from 'react';
+import { ArrowBack, ArrowForward } from '@mui/icons-material';
+export interface PaginationProps {
+  isPlaceholderData: boolean;
+  prevPage: [] | number[];
+  onPrevClick: () => void;
+  onNextClick: () => void;
+}
+const Pagination: FC<PaginationProps> = ({ isPlaceholderData, prevPage, onPrevClick, onNextClick }) => {
+  return (
+    <div className="p-2 md:px-8 md:py-4 flex gap-1 justify-end items-center w-full ">
+      <IconButton onClick={onPrevClick} disabled={isPlaceholderData || prevPage === undefined || prevPage.length == 0}>
+        <ArrowBack />
+      </IconButton>
+      <IconButton onClick={onNextClick} disabled={isPlaceholderData}>
+        <ArrowForward />
+      </IconButton>
+    </div>
+  );
+};
+
+export default Pagination;
