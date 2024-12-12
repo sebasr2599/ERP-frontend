3407284fa5075a5cbf81b5bbbb5283e78d918844
Sebastian Resendiz <sebas.r.2599@gmail.com>
2024-12-11 21:11:01 -0600

    Added Category Slider into custom component

---
+++ src/components/CategorySlider/CategorySlider.tsx
@@ -0,0 +1,23 @@
+import { UseQueryResult } from '@tanstack/react-query';
+import { ToggleButton, ToggleButtonGroup } from '@mui/material';
+import { FC } from 'react';
+export interface OrderRowProps {
+  categoriesQuery: UseQueryResult<Category[], Error>;
+  selectedCategory: number | undefined;
+  onCategorySelect: (event: React.MouseEvent<HTMLElement>, selected: number) => void;
+}
+const CategorySlider: FC<OrderRowProps> = ({ categoriesQuery, selectedCategory, onCategorySelect }) => {
+  return (
+    <div className="overflow-x-auto scroll-pl-4 w-3/4 md:w-1/2 ">
+      <ToggleButtonGroup value={selectedCategory} exclusive onChange={onCategorySelect} className="flex">
+        {categoriesQuery.data?.map((category) => (
+          <ToggleButton key={category.id} value={category.id || 0} className="flex-shrink-0">
+            {category.name}
+          </ToggleButton>
+        ))}
+      </ToggleButtonGroup>
+    </div>
+  );
+};
+
+export default CategorySlider;
